655a27792e799117eade092da7f002266e2b94ce
sonali kharade <kharadesonali05@gmail.com>
2023-10-07 14:31:43 +0530

    update

---
+++ script.js
@@ -0,0 +1,40 @@
+var menu= document.querySelector('#menu-btn');
+var navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+   document.querySelector('#close-login-form').onclick=()=>{
+   document.querySelector('.login-form-container').classList.remove('active');
+}
+
+document.querySelector('#login-btn').onclick=()=>{
+    document.querySelector('.login-form-container').classList.toggle('active');
+ }
+window.onscroll = () =>{
+    if(window.scrollY > 0){
+           document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+window.onload = () =>{
+    if(window.scrollY>0){
+           document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+   
+}
+
+
+
+
+
+
+
+
